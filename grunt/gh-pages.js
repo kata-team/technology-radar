75370b3d762729d63ad4ddbc060fde99a367058c
Marco Montalbano <marcomontalbano.work@gmail.com>
2017-01-25 09:10:21 +0100

    Changed deploy message and fixed codacy issues.

--- grunt/gh-pages.js
+++ grunt/gh-pages.js
@@ -4,17 +4,18 @@ module.exports = {
         branch: 'gh-pages',
         user: {
             name: 'Marco Montalbano',
-            email: 'marcomontalbano.work@gmail.com'
+            email: 'marcomontalbano.work@gmail.com',
         },
-        message: 'Deploy to GitHub Pages.'
+        message: 'Deploy to GitHub Pages.',
     },
     local: {
-        src: ['**']
+        src: ['**'],
     },
     travis: {
         options: {
-            repo: 'https://' + process.env.GH_TOKEN + '@github.com/kata-team/technology-radar.git'
+            repo: `https://${process.env.GH_TOKEN}@github.com/kata-team/technology-radar.git`,
+            message: 'Deploy to GitHub Pages from Travis CI.',
         },
-        src: ['**']
+        src: ['**'],
     }
 };
