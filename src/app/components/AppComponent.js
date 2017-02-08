df9004185f0ff1dbffc77b25a183e6274b103aee
Marco Montalbano <marcomontalbano.work@gmail.com>
2017-02-08 14:55:44 +0100

    Loaded items on page load.

--- src/app/components/AppComponent.js
+++ src/app/components/AppComponent.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import NavbarComponent from './NavbarComponent';
 import ResultComponent from './ResultComponent';
+import SearchActions from '../actions/SearchActions';
 
 export default class AppComponent extends Component {
+
+    componentDidMount() {
+        SearchActions.changeCriteria('');
+    }
 
     render() {
         return (
