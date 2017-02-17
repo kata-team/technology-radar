005b8e667d0e2db624f43ecddeb564dd4d0f158c
Marco Montalbano <marcomontalbano.work@gmail.com>
2017-02-17 09:39:52 +0100

    Added 'tags' to card footer.

--- src/app/class/Item.js
+++ src/app/class/Item.js
@@ -1,10 +1,10 @@
 export default class Item {
-    constructor({ name = '', description = '', category = '', status = '', url = '', tags = [] }) {
+    constructor({ name = '', description = '', category = '', status = '', url = '', tags = '' }) {
         this.name = name;
         this.description = description;
         this.category = category;
         this.status = status;
-        this.tags = tags;
+        this.tags = tags.split(',');
         this.url = url;
     }
 }
