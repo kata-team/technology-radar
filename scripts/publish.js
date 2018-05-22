734f2edbae8688b129e9ba88f2c78d8a931018ad
Marco Montalbano <marcomontalbano.work@gmail.com>
2018-05-23 00:17:59 +0200

    Added 'ruby' configuration for private website.

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,13 +2,16 @@ const ghpages = require('gh-pages');
 const config = require('../package.json');
 const domain = process.env.GH_TOKEN ? `${process.env.GH_TOKEN}@github.com` : 'github.com';
 
+const branch = process.argv[2] || 'gh-pages';
+
 ghpages.publish('build', {
     dotfiles: false,
-    branch: 'gh-pages',
+    branch: branch,
     repo: config.repository.url.replace('github.com', domain),
     message: 'Publish website',
-    silent: true
+    //silent: true
 }, function (err) {
+    console.log(err);
     if (err) {
         throw new Error(err.message);
     } else {
