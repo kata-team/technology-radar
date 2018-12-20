110ba232fc2d4c8fba33af22d487ecc0b478a663
Marco Montalbano <marcomontalbano.work@gmail.com>
2018-12-20 22:40:27 +0100

    Add version number to the footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import AppComponent from './app/components/AppComponent';
+import packageJson from '../package.json';
+
+document.getElementById('version').innerHTML = `v ${packageJson.version}`;
 
 ReactDOM.render(
     <AppComponent />
