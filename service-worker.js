029dacc40a66bdc8ecb7d412659afbd54118cb70
Travis CI User <travis@example.org>
2019-01-08 09:16:40 +0000

    Publish website

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c14193fea4306bf90368f2cd63869242.js"
+  "./precache-manifest.407dd9ff847be68e7c4a60b62b4c80de.js"
 );
 
 workbox.clientsClaim();
